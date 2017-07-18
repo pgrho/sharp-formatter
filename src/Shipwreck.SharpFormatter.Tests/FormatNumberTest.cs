a665b7dba7e5265b3e4e9601f00d1b34f4a78d80
pgrho <shipwreck@live.jp>
2017-07-18 14:33:03 +0900

    Initial commit

---
+++ src/Shipwreck.SharpFormatter.Tests/FormatNumberTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.PhantomJS;
+using System;
+
+namespace Shipwreck.SharpFormatter.Tests
+{
+    [TestClass]
+    public class FormatNumberTest
+    {
+        private static PhantomJSDriver _Driver;
+
+        private PhantomJSDriver CreateWebDriver()
+        {
+            var d = new PhantomJSDriver();
+            d.Navigate().GoToUrl(new Uri(new Uri(GetType().Assembly.Location), "test.html").ToString());
+            return d;
+        }
+
+        private void Test<T>(T value, string format)
+            where T : struct, IFormattable, IConvertible
+        {
+            if (_Driver == null)
+            {
+                _Driver = CreateWebDriver();
+            }
+            var s = _Driver.ExecuteScript(string.Format("return Shipwreck.SharpFormatter.formatNumber({0:r}, '{1}');", value.ToDouble(null), format));
+            Assert.AreEqual(value.ToString(format, null), s);
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            _Driver?.Close();
+            _Driver = null;
+        }
+
+        [TestMethod]
+        public void FormattNumber_D()
+            => Test(123, "d");
+
+        [TestMethod]
+        public void FormattNumber_D2()
+            => Test(123, "d2");
+
+        [TestMethod]
+        public void FormattNumber_D6()
+            => Test(123, "d6");
+
+        [TestMethod]
+        public void FormattNumber_E()
+            => Test(Math.PI * Math.Pow(2, 48), "e");
+
+        [TestMethod]
+        public void FormattNumber_E3()
+            => Test(Math.PI * Math.Pow(2, 48), "e3");
+
+        [TestMethod]
+        public void FormattNumber_E20()
+            => Test(Math.PI * Math.Pow(2, 48), "e20");
+
+        [TestMethod]
+        public void FormattNumber_F()
+            => Test(Math.PI, "f");
+
+        [TestMethod]
+        public void FormattNumber_F3()
+            => Test(Math.PI, "f3");
+
+        [TestMethod]
+        public void FormattNumber_F6()
+            => Test(Math.PI, "f6");
+    }
+}
---
+++ src/Shipwreck.SharpFormatter/SharpFormatter.ts
@@ -0,0 +1,53 @@
+module Shipwreck {
+    export class SharpFormatter {
+        public static formatNumber(value: number, format: string) {
+            if (!format) {
+                return SharpFormatter.formatNumber(value, "g");
+            }
+            if (/^[Cc][0-9]*$/.test(format)) {
+            } else if (/^[Dd][0-9]*$/.test(format)) {
+                var length = format.length === 1 ? 0 : parseInt(format.substring(1), 10);
+                var r = value.toFixed();
+                while (r.length < length) {
+                    r = '0' + r;
+                }
+                return r;
+            } else if (/^[Ee][0-9]*$/.test(format)) {
+                return this._formatExponentialNumber(value, format.length === 1 ? 6 : parseInt(format.substring(1), 10), format.charAt(0) === 'E');
+            } else if (/^[Ff][0-9]*$/.test(format)) {
+                var length = format.length === 1 ? 2 : parseInt(format.substring(1), 10);
+                return value.toFixed(length);
+            } else if (/^[Gg][0-9]*$/.test(format)) {
+                var length = format.length === 1 ? 15 : parseInt(format.substring(1), 10);
+                var r = SharpFormatter._formatExponentialNumber(value, length, false);
+                if (/[Ee]-?[0-9]$/.test(r)) {
+                    var i = r.length - RegExp.length;
+                    var e = parseInt(format.substring(i + 1), 10);
+                    if (-5 < e && e < length) {
+                        return value.toFixed(length);
+                    }
+                }
+                return format.charAt(0) === 'G' ? r.toUpperCase() : r;
+                //} else if (/^[Nn][0-9]*$/.test(format)) {
+                //    var length = format.length === 1 ? 2 : parseInt(format.substring(1), 10);
+                //    var r = value.toFixed(length);
+                //    var dp = r.indexOf('.');
+                //} else if (/^[Pp][0-9]*$/.test(format)) {
+                //    // %
+                //} else if (/^[Rr][0-9]*$/.test(format)) {
+                //} else if (/^[Xx][0-9]*$/.test(format)) {
+            } else if (/^.$/) {
+                throw "Invalid format";
+            } else {
+                throw "Not implemented";
+                // custom:
+            }
+        }
+        private static _formatExponentialNumber(value: number, length: number, capital: boolean): string {
+            length = length >= 0 ? length : 6;
+            var r = value.toExponential(length);
+            r = r.replace(/[-+][0-9]{1,2}$/, m => m.charAt(0) + (m.length === 2 ? "00" : "0") + m.substring(1));
+            return capital ? r.toUpperCase() : r;
+        }
+    }
+}
